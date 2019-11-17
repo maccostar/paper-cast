1e2ad15df093937b4ef31334915b0bf857085a93
三島明恵 <myunghye.kor@gmail.com>
2019-11-17 11:57:13 +0900

    printer直列処理

--- server/api/deliveryDate.js
+++ server/api/deliveryDate.js
@@ -6,10 +6,18 @@ module.exports = async (req, res) => {
     case 'put':
       const printers = getPrinters()
       const day = req.query.deliveryDate
-      await Promise.all(printers.map(email => print(email, `./static/printers/${email}/${day}.jpg`)))
+      console.log(printers)
+
+      for (let i = 0; i < printers.length; i += 1) {
+        await print(printers[i], `./static/printers/${printers[i]}/${day}.jpg`).catch(e => console.log(e))
+      }
+      // await Promise.all(printers.map(email => print(email, `./static/printers/${email}/${day}.jpg`)))
 
       if (day === '30') {
-        await Promise.all(printers.map(email => print(email, `./static/printers/${email}/31.jpg`)))
+        // await Promise.all(printers.map(email => print(email, `./static/printers/${email}/31.jpg`)))
+        for (let i = 0; i < printers.length; i += 1) {
+          await print(printers[i], `./static/printers/${printers[i]}/31.jpg`).catch(e => console.log(e))
+        }
       }
 
       res.sendStatus(204)
