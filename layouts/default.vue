7552ed2b659bcecab6cc89bea87f66377ff36031
s-iijima0113 <everyth1ng.n1ce.St01@gmail.com>
2019-11-17 10:16:22 +0900

    ヘッダー作成

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,10 +1,37 @@
 <template>
-  <v-app dark>
-    <nuxt />
-  </v-app>
+  <v-card
+    color="grey lighten-4"
+    flat
+    height="200px"
+    tile>
+    <v-toolbar
+      prominent
+      extended
+      :dark="true">
+      <div class="logo-image" />
+
+      <v-spacer></v-spacer>
+
+      <v-toolbar-title>
+        配信先一覧&emsp;&emsp;&emsp;&emsp;&emsp;
+      </v-toolbar-title>
+
+      <v-toolbar-title>配信日選択&emsp;&emsp;&emsp;
+      </v-toolbar-title>
+
+    </v-toolbar>
+  </v-card>
 </template>
 
 <script>
 export default {
 }
 </script>
+
+<style scoped>
+.logo-image{
+  background: center / cover url('~assets/image/papercast-logo.png');
+  height: 180px;
+  width: 220px;
+}
+</style>
